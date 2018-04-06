c90f41b1b2f95d9da963a90186fd950f566ff56d
Justin <justinwhitaker2016@gmail.com>
2018-04-06 16:20:08 -0700

    made sure problem1 and problem2again were present and accounted for to avoid problems with the merge

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var sumNum = [(a+b),'The sum of ' + a + ' and ' + b + ' is ' + (a+b) + '.'];
+  return sumNum;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,7 +28,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  var multNum = [a*b,'The product of ' + a + ' and ' + b + ' is ' + a*b + '.'];
+  var multNum = [(a*b),'The product of ' + a + ' and ' + b + ' is ' + (a*b) + '.'];
   return multNum;
 }
 
