af685307b855038d683c16527cbd0beb9e53ee63
Justin <justinwhitaker2016@gmail.com>
2018-04-06 15:56:59 -0700

    Manually added problem1 because it wasn't showing up after pushing problem1 branch, merging, then pulling new master back down

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var sumNum = [a+b,'The sum of ' + a + ' and ' + b + ' is ' + (a+b) + '.'];
+  return sumNum;
 }
 
 // Here is the test for sum(); uncomment it to run it
